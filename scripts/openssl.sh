73a99317fc9eedf5db9179f5c16b953eb4edf8a7
qmuntal <qmuntaldiaz@microsoft.com>
2022-02-07 18:48:31 +0100

    copy crypto includes

--- scripts/openssl.sh
+++ scripts/openssl.sh
@@ -43,5 +43,7 @@ cd "openssl-$version"
 make build_libs
 
 rm -rf /usr/include/openssl
+rm -rf /usr/include/crypto
 cp -r -L ./include/openssl /usr/include
+cp -r -L ./include/crypto /usr/include
 cp -H ./libcrypto.so "/usr/lib/libcrypto.so.${version}"
