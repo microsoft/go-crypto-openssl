9fafecd26c83a05018bd5a64541c488c2cb247db
Quim Muntal <qmuntaldiaz@microsoft.com>
2022-03-29 15:10:31 +0000

    make SHA calls allocation-free

--- openssl/openssl.go
+++ openssl/openssl.go
@@ -258,3 +258,14 @@ func bnToBig(bn C.GO_BIGNUM_PTR) *big.Int {
 	n := C.go_openssl_BN_bn2bin(bn, base(raw))
 	return new(big.Int).SetBytes(raw[:n])
 }
+
+// noescape hides a pointer from escape analysis. noescape is
+// the identity function but escape analysis doesn't think the
+// output depends on the input. noescape is inlined and currently
+// compiles down to zero instructions.
+// USE CAREFULLY!
+//go:nosplit
+func noescape(p unsafe.Pointer) unsafe.Pointer {
+	x := uintptr(p)
+	return unsafe.Pointer(x ^ 0)
+}
