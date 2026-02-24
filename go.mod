28fbca3def1036727e0ac60a66e2545c0c4e8591
michelle-clayton-work <mclayton+github@microsoft.com>
2026-02-24 11:56:13 -0800

    Update hashclone (#381)
    Co-authored-by: Michelle Clayton <mclayton@microsoft.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/golang-fips/openssl/v2
 
-go 1.24
+go 1.25
--- hashclone.go
+++ hashclone.go
@@ -1,4 +1,4 @@
-//go:build !go1.25 && !cmd_go_bootstrap
+//go:build !cmd_go_bootstrap
 
 package openssl
 
@@ -6,9 +6,4 @@ import (
 	"hash"
 )
 
-// HashCloner is an interface that defines a Clone method.
-type HashCloner interface {
-	hash.Hash
-	// Clone returns a separate Hash instance with the same state as h.
-	Clone() (HashCloner, error)
-}
+type HashCloner = hash.Cloner
